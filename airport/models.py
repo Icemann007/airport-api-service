a32f5e67df0c34728d52fa4c99428e907ed20460
Nazar <nazargorodyko@gmail.com>
2025-05-19 17:55:40 +0300

    Created airport and user models with initial migrations

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,160 @@
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    closest_big_city = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        "Airport",
+        on_delete=models.CASCADE,
+        related_name="source_routes",
+    )
+    destination = models.ForeignKey(
+        "Airport",
+        on_delete=models.CASCADE,
+        related_name="destination_routes",
+    )
+    distance = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["source", "destination"], name="unique_route_source_destination"
+            )
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.source.name} - {self.destination.name}"
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=64)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(
+        "AirplaneType",
+        on_delete=models.CASCADE,
+        related_name="airplanes",
+    )
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        "Route",
+        on_delete=models.CASCADE,
+        related_name="flights",
+    )
+    airplane = models.ForeignKey(
+        "Airplane",
+        on_delete=models.CASCADE,
+        related_name="flights",
+    )
+    crew = models.ManyToManyField(
+        Crew,
+        related_name="flights",
+        blank=True,
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-departure_time"]
+
+    def __str__(self) -> str:
+        return f"Flight: {self.id} on {str(self.route)}"
+
+    def clean(self):
+        if self.arrival_time <= self.departure_time:
+            raise ValidationError("Arrival time must be after departure time")
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(
+        "Flight",
+        on_delete=models.CASCADE,
+        related_name="tickets",
+    )
+    order = models.ForeignKey(
+        "Order",
+        on_delete=models.CASCADE,
+        related_name="tickets",
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["flight", "row", "seat"], name="unique_ticket_row_seat_flight"
+            )
+        ]
+        ordering = ["row", "seat"]
+
+    def __str__(self) -> str:
+        return (
+            f"Ticket {self.id} for Flight {self.flight}, seat: {self.row} - {self.seat}"
+        )
+
+    def clean(self):
+        if not (1 <= self.row <= self.flight.airplane.rows):
+            raise ValidationError(
+                {
+                    "row": f"row must be in range [1, {self.flight.airplane.rows}], not {self.row}"
+                }
+            )
+
+        if not (1 <= self.seat <= self.flight.airplane.seats_in_row):
+            raise ValidationError(
+                {
+                    "seat": f"seat must be in range [1, {self.flight.airplane.seats_in_row}], not {self.seat}"
+                }
+            )
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return f"Order {self.id} by {self.user}"
