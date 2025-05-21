35de951a8eb5aa308ed8314ef14686e72ae4c774
Nazar <nazargorodyko@gmail.com>
2025-05-21 21:38:25 +0300

    Added nested create method for Order and Ticket

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
@@ -176,6 +177,14 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ["id", "tickets", "created_at"]
+
+    def create(self, validated_data):
+        with transaction.atomic:
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(**ticket_data, order=order)
+            return order
 
 
 class OrderListSerializer(OrderSerializer):
