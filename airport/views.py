fc4e00a2724f3ff38b2e7ab3cfbb312c6cdcdb14
Nazar <nazargorodyko@gmail.com>
2025-05-21 16:35:08 +0300

    Added perform_create to Order

--- airport/views.py
+++ airport/views.py
@@ -77,3 +77,6 @@ class OrderViewSet(viewsets.ModelViewSet):
             return OrderListSerializer
 
         return OrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
