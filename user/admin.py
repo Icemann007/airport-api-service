8d7bf0802d2f557c0caed2eb309cd26d4d0114a4
Nazar <nazargorodyko@gmail.com>
2025-05-26 18:15:49 +0300

    Registered custom user model with customized admin

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from user.models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password", "username")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "username", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "username", "first_name", "last_name")
+    ordering = ("username",)
